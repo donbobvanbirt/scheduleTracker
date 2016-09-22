b8909bf805a32a40b11352567814549ee995d4cf
Donovan <donovan6925@gmail.com>
2016-09-21 20:29:29 -0700

    added edit and delete buttons

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,25 @@ const App = React.createClass({
     }, console.log("item", item));
   },
 
+  edit(newState) {
+    this.setState({
+      items: newState
+    }, () => console.log("editing state"))
+  },
+
+  removeItem(id) {
+    const { items } = this.state;
+    let newItems = items.filter(item => item.id !== id);
+    this.edit(newItems);
+  },
+
   render() {
     const { items } = this.state;
     return (
       <div className="container">
         <h1>Schedule Tracker</h1>
         <ItemForm newItem={this.newItem}/>
-        <ItemList items={items}/>
+        <ItemList items={items} edit={this.edit} removeItem={this.removeItem}/>
       </div>
     )
   }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,49 +1,104 @@
 const React = require('react');
 
-const ItemList = props => {
-  const { items } = props;
+const ItemList = React.createClass({
+  getInitialState() {
+    return {
+      editing: ''
+    }
+  },
 
-  console.log("items:", items);
+  editItem(id) {
+    console.log('editing', id);
+    this.setState({
+      editing: id
+    })
+  },
 
-  let Items = items.map((item, i) => {
+  onChange(name, index) {
+    const { title, start, end } = this.refs;
+    const { edit, items } = this.props;
+    let newItems = items;
+
+    switch (name) {
+      case "title":
+        newItems[index][name] = title.value;
+        edit(newItems);
+        console.log("editing title");
+      break;
+      case "start":
+        newItems[index][name] = start.value;
+        edit(newItems);
+      break;
+      case "end":
+        newItems[index][name] = end.value;
+        edit(newItems);
+      break;
+    }
+  },
+
+  endEdit() {
+    this.setState({
+      editing: ''
+    })
+  },
+
+  render() {
+    const { items, edit, removeItem } = this.props;
+    const { editing } = this.state;
+
+    console.log("items:", items);
+
+    let tableBody = items.map((item, index) => {
+      if (item.id === this.state.editing) {
+        return (
+          <tr key={item.id}>
+            <td>
+              <input ref="title" type="text" value={item.title} onChange={this.onChange.bind(this, "title", index)} required/>
+            </td>
+            <td>
+              <input ref="start" type="date" value={item.start} onChange={this.onChange.bind(this, "start", index)} />
+            </td>
+            <td>
+              <input ref="end" type="date" value={item.end} onChange={this.onChange.bind(this, "end", index)} />
+            </td>
+            <td>{item.submitted}</td>
+            <td><button onClick={() => {this.endEdit()}} className="btn btn-sm btn-success">Save</button></td>
+            <td><button onClick={removeItem.bind(null, item.id)}  className="btn btn-sm btn-danger"><i className="fa fa-trash"></i></button></td>
+          </tr>
+        )
+      } else {
+        return (
+          <tr key={item.id}>
+            <td>{item.title}</td>
+            <td>{item.start}</td>
+            <td>{item.end}</td>
+            <td>{item.submitted}</td>
+            <td><button onClick={() => {this.editItem(item.id)}} className="btn btn-sm btn-success"><i className="fa fa-edit"></i></button></td>
+            <td><button onClick={removeItem.bind(null, item.id)}  className="btn btn-sm btn-danger"><i className="fa fa-trash"></i></button></td>
+          </tr>
+        )
+      }
+    })
+
     return (
-      <tr key={i}>
-        <td>{item.title}</td>
-        <td>{item.start}</td>
-        <td>{item.end}</td>
-        <td>{item.submitted}</td>
-        <td>Edit</td>
-        <td>Delete</td>
-      </tr>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Submitted</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableBody}
+        </tbody>
+
+      </table>
     )
-  })
-
-  return (
-
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Start Date</th>
-          <th>End Date</th>
-          <th>Submitted</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Items}
-      </tbody>
-
-    </table>
-    //
-    //
-    // <ul>
-    //   {items.map((item, i) => (
-    //     <li key={i}></li>
-    //   ))}
-    // </ul>
-  )
-}
+  }
+});
 
 module.exports = ItemList;
